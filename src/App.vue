a8d713e7e4fd7506c74292ca1583ce6bb3dc1a15
Adam4ever <adam4ever@gmail.com>
2023-05-28 10:04:55 -0300

    Reconfigure initial setup
    - fix TS issues
    - update prettier
    - remove boilerplating

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
@@ -11,5 +10,4 @@ import { RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
